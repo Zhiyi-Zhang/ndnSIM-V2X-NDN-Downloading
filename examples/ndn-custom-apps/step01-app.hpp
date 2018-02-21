fdda8477b17513ae6d7e9021913d80c7411c5e15
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2018-02-20 21:26:23 -0800

    add custom app and r script

---
+++ examples/ndn-custom-apps/step01-app.hpp
@@ -0,0 +1,62 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/**
+ * Copyright (c) 2011-2015  Regents of the University of California.
+ *
+ * This file is part of ndnSIM. See AUTHORS for complete list of ndnSIM authors and
+ * contributors.
+ *
+ * ndnSIM is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * ndnSIM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ndnSIM, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Zhiyi Zhang, Your name here
+ **/
+
+
+#ifndef STEP01_APP_H_
+#define STEP01_APP_H_
+
+#include "ns3/ndnSIM/apps/ndn-app.hpp"
+
+namespace ns3 {
+
+/**
+ * @brief A simple custom application
+ */
+class Step01App : public ndn::App {
+public:
+  // register NS-3 type "Step01App"
+  static TypeId
+  GetTypeId();
+
+  // (overridden from ndn::App) Processing upon start of the application
+  virtual void
+  StartApplication();
+
+  // (overridden from ndn::App) Processing when application is stopped
+  virtual void
+  StopApplication();
+
+  // (overridden from ndn::App) Callback that will be called when Interest arrives
+  virtual void
+  OnInterest(std::shared_ptr<const ndn::Interest> interest);
+
+  // (overridden from ndn::App) Callback that will be called when Data arrives
+  virtual void
+  OnData(std::shared_ptr<const ndn::Data> contentObject);
+
+private:
+  void
+  SendInterest();
+};
+
+} // namespace ns3
+
+#endif // STEP01_APP_H_
---
+++ rtt-extraction.R
@@ -0,0 +1,39 @@
+rawData <- readLines("../../log.txt")
+
+dataPattern <- "^\\+([.0-9]*).*DATA for ([0-9]*)$"
+dataData <- regmatches(rawData, gregexpr(dataPattern, rawData))
+dataData <- dataData[grep(dataPattern, dataData)]
+for (i in 1:length(dataData)) {
+  dataData[i] <- sub(dataPattern, "\\1 \\2", dataData[i])
+}
+
+interestPattern <- "^\\+([.0-9]*).*Interest for ([0-9]*)$"
+interestData <- regmatches(rawData, gregexpr(interestPattern, rawData))
+interestData <- interestData[grep(interestPattern, interestData)]
+for (i in 1:length(interestData)) {
+  interestData[i] <- sub(interestPattern, "\\1 \\2", interestData[i])
+}
+
+
+df <- data.frame(sending = numeric(length(dataData)),
+                 recieving = numeric(length(dataData)),
+                 rtt = numeric(length(dataData)),
+                 stringsAsFactors = FALSE)
+
+for (i in 1:length(dataData)) {
+  item <- strsplit(as.character(dataData[i]), " ")
+  index <- as.numeric(item[[1]][2])
+  recieveTime <- as.numeric(item[[1]][1])
+  for (j in length(interestData):1) {
+    item2 <- strsplit(as.character(interestData[j]), " ")
+    index2 <- as.numeric(item2[[1]][2])
+    sendTime <- as.numeric(item2[[1]][1])
+    if (index2 - index == 0) {
+      df$sending[index] <- sendTime
+      df$recieving[index] <- recieveTime
+      df$rtt[index] <- recieveTime - sendTime
+      break
+    }
+  }
+}
+
